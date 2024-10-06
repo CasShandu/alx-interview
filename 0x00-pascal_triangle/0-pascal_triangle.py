ba11e3b148ce9fc357f6feee687cfa6cacae566e
root <root@Mpumie-PC.lan>
2024-10-06 22:09:45 +0200

    pascal triangular numeric

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,18 @@
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = []
+
+    for i in range(n):
+        # Start each row with 1
+        row = [1] * (i + 1)
+
+        # Each triangle element (except the first and last) is the sum of the two elements above it
+        for j in range(1, i):
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+
+        triangle.append(row)
+
+    return triangle
+
---
+++ 0x00-pascal_triangle/README.md
@@ -0,0 +1,1 @@
+make a numeric pascal triangle
