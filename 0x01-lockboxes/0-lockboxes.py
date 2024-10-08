c702074295f845d26f2f2c299abd14447de16c5f
root <root@Mpumie-PC.lan>
2024-10-08 14:02:38 +0200

    pycode issues

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,4 +1,16 @@
+#!/usr/bin/python3
+
 def canUnlockAll(boxes):
+    """
+    Determines if all boxes can be unlocked.
+
+    Args:
+        boxes (list): A list of lists where each inner list contains the keys
+                      that can unlock other boxes.
+
+    Returns:
+        bool: True if all boxes can be unlocked, False otherwise.
+    """
     n = len(boxes)
     unlocked = [False] * n
     unlocked[0] = True
